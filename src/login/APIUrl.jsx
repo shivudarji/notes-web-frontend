8429955f8ea74cec7418fba8e5fa093bc1ec2f57
shivudarji <shivanidarji5@gmail.com>
2026-01-04 22:18:08 +0530

    Add untracked files to gitignore

--- src/login/APIUrl.jsx
+++
@@ -1,6 +0,0 @@
-// import axios from "axios";
-
-const APIUrl = "https://notes-web-backend.vercel.app/api/v1";
-// "http://192.168.3.203:5000/api/v1"; // Replace with your API base URL
-
-export default APIUrl;
